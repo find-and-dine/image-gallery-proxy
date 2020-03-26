f263cb290bd97dca3217c7089324257394d3c5de
Phonxay Champasouk <52714374+phonxaychampasouk@users.noreply.github.com>
2020-03-25 19:10:28 -0700

    Render 3001's bundle to html. Have positioned static fillers to html page

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const path = require('path');
-
+const PORT = 3000;
 const app = express();
 
-app.use('/:id', express.static(path.resolve(__dirname, '..', 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/:id', express.static(path.resolve(__dirname, '..', 'public')));
 
 app.listen(PORT, () => console.log('Listening on port', PORT));
+
+
+
+
+
 
 module.export=app;
